661ca69300b3dffb9beb75f2f5f676f3aa4d8814
Alloy00 <viniciuscisneiros03@gmail.com>
2023-05-13 09:55:11 -0300

    0.2 version

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(' mongodb://127.0.0.1:27017', { useNewUrlParser: true }).then(
+mongoose.connect(' mongodb://127.0.0.1:27017/tenis', { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)});
 
